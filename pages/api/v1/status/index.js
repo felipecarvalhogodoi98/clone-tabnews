73c82b489feb7884768fcf19640fd90632489ed4
felipecarvalhogodoi98 <felipecarvalhogodoi98@gmail.com>
2026-03-09 21:05:44 -0300

    finish status API

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,9 +1,33 @@
-import database from "../../../../infra/database";
+import database from "infra/database";
 
 const status = async (req, res) => {
-  const result = await database.query("SELECT 1 + 1 as SUM;");
-  console.log(result.rows);
-  res.status(200).send({ status: "ok" });
-}
+  const updateAt = new Date().toISOString();
+  const postgresVersionQuery = await database.query("SHOW server_version;");
+  const postgresVersion = postgresVersionQuery.rows[0].server_version;
+
+  const postgresMaxConnectionsQuery = await database.query(
+    "SELECT current_setting('max_connections')::int AS max_connections;",
+  );
+  const postgresMaxConnections =
+    postgresMaxConnectionsQuery.rows[0].max_connections;
+
+  const postgresOpenedConnectionsQuery = await database.query({
+    text: "SELECT COUNT(*)::int FROM pg_stat_activity WHERE datname = $1;",
+    values: [process.env.POSTGRES_DB],
+  });
+  const postgresOpenedConnections =
+    postgresOpenedConnectionsQuery.rows[0].count;
+
+  res.status(200).send({
+    update_at: updateAt,
+    dependencies: {
+      database: {
+        version: postgresVersion,
+        max_connections: postgresMaxConnections,
+        opened_connections: postgresOpenedConnections,
+      },
+    },
+  });
+};
 
 export default status;
--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -1,4 +1,21 @@
-test('GET /api/v1/status', async () => {
-  const response = await fetch('http://localhost:3000/api/v1/status');
-  expect(response.status).toBe(200);
+describe("GET /api/v1/status", () => {
+  it("should return the status of the application", async () => {
+    const response = await fetch("http://localhost:3000/api/v1/status");
+    expect(response.status).toBe(200);
+
+    const data = await response.json();
+
+    console.log(data);
+
+    const updateAt = new Date(data.update_at);
+    const now = new Date();
+    const diff = now.getTime() - updateAt.getTime();
+    const diffInSeconds = diff / 1000;
+    expect(diffInSeconds).toBeLessThan(1000);
+
+    const database = data.dependencies.database;
+    expect(database.version).toBe("16.0");
+    expect(database.max_connections).toBe(100);
+    expect(database.opened_connections).toBe(1);
+  });
 });
